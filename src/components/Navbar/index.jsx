31f30f199e188df6226de43df1bc1900a9600d6f
madeleineenberg <madeleine_enberg@hotmail.com>
2022-01-10 10:09:50 +0100

    navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.scss";
+
+export default function Navbar() {
+    return (
+        <section className="navbar">
+            <div>menu</div>
+            <div>Madeleine Enberg</div>
+            <div>contact</div>
+        </section>
+    )
+}
