78a042126b3e1e73e1ce5b1446873aeb421361d0
madeleineenberg <madeleine_enberg@hotmail.com>
2022-02-09 20:32:52 +0100

    add: about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
 import "./style.scss";
 import SectionHeader from "../SectionHeader/index";
+import gsap from "gsap";
+import SplitText from "../../utils/Split3.min.js";
+import useOnScreen from "../../hooks/useOnScreen";
+import cn from 'classnames';
 
 export default function About() {
+  const ref = useRef(null);
+  const [reveal, setReveal] = useState(false);
+  const onScreen = useOnScreen(ref);
+
+  useEffect(() => {
+    if(onScreen) setReveal(onScreen);
+  }, [onScreen])
+
+  useEffect(() => {
+    if(reveal) {
+      
+      const split = new SplitText("#headline", {
+        type: "lines",
+      });
+  
+      gsap.to(split.lines, {
+        duration: 1,
+        y: -20,
+        opacity: 1,
+        stagger: 0.1,
+        ease: "power4.out",
+      });
+
+    }
+  }, [reveal]); 
+
   return (
-    <section className={"about-section"} data-scroll-section>
+    <section className={cn("about-section", {"is-reveal": reveal})} data-scroll-section>
       <SectionHeader title="about" />
-      <p id="headline">
+      <p ref={ref} id="headline" className={cn({ "is-reveal": reveal })}>
         "Her style is beautiful and somewhat dark and moody, but intensely
         dreamlike. If it was music it would certainly translate to Portishead on
         film. Her work consists of lots of blue and purple tones and some
